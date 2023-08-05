830e184ee706157a09b7f24f0a48c148c6aebf03
CarEnthusiastJohn50 <129770852+pow4you@users.noreply.github.com>
2023-08-05 20:52:22 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,7 @@
 export default defineAppConfig({
     socials: [
         {name: "github", icon:"ph:github-logo-thin", url: "https://github.com/pow4you"},
+        {name: "discord", icon:"ph:discord-logo-thin", url: "https://discord.com/users/powowocat"},
         {name: "instagram", icon:"ph:instagram-logo-thin", url: "https://instagram.com/powcosplay"},
         {name: "twitter", icon:"ph:twitter-logo-thin", url: "https://twitter.com/PowowoCat"},
         {name: "bsky", icon:"ph:cloud-sun-thin", url: "https://bsky.app/profile/pow.bsky.social"},
