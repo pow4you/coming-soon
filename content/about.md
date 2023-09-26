2f629ef4216f03b495f2f12f2a9edf2c19363c79
CarEnthusiastJohn50 <129770852+pow4you@users.noreply.github.com>
2023-09-26 20:08:05 +0200

    initial repo

---
+++ content/about.md
@@ -0,0 +1,9 @@
+---
+layout: content_layouts
+---
+
+# About
+
+This page is using the `layouts/content_layouts/custom.vue` layout.
+
+[Back home](/)
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div> Default </div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+</style>
